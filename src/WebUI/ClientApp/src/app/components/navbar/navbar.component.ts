d8dc5b8d0cc8302653df40a75d5d4a49c4ff0ac3
fernandohermida <fernando.hermida@greenfinch.ie>
2020-05-20 00:29:34 -0300

    Add Angular Material navigation bar and layout

---
+++ src/WebUI/ClientApp/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { NavMenuItem } from 'src/app/models/models';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  menu = [
+    { Label: 'Dashboard', Target: '/dashboard' },
+    { Label: 'Table', Target: '/table' },
+    { Label: 'Counter', Target: '/counter' },
+    { Label: 'Fetch data', Target: '/fetch-data' },
+    { Label: 'Todo', Target: '/todo' }
+    ] as NavMenuItem[];
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
