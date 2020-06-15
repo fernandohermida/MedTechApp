2c65cc854a088be5f39c0259bafd1c2b4b494459
fernandohermida <fernando.hermida@greenfinch.ie>
2020-06-15 12:43:17 -0300

    add Devices

---
+++ src/Domain/Entities/Device.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace MedTechApp.Domain.Entities
+{
+    class Device
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Serial { get; set; }
+
+        public Patient LinkPatienat { get; set; }
+    }
+}
